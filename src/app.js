b0ddd8c2f4660da412f5f587ee41819aa6f1d730
shivam <shshivampandey@thoughtwin.com>
2023-12-11 17:11:26 +0530

    add origin

--- src/app.js
+++ src/app.js
@@ -1,25 +1,29 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
 const port = 5002;
 
-const dbConfig = require("../config/secret");
-const todo = require("../routes/todoRoutes");
-const auth = require("../routes/authRoutes");
-const user = require("../routes/userRourtes");
+const dbConfig = require('../config/secret');
+const todo = require('../routes/todoRoutes');
+const auth = require('../routes/authRoutes');
+const user = require('../routes/userRourtes');
 
 app.use(express.json());
 
-mongoose.connect(dbConfig.url, { useNewUrlParser: true })
-  
-app.use(cors("*"));
+mongoose.connect(dbConfig.url, { useNewUrlParser: true });
 
-app.use("/api/todo",todo);
-app.use("/api/todo", auth);
-app.use("/api/todo", user);
+app.use(
+  cors({
+    origin: '*',
+  })
+);
+
+app.use('/api/todo', todo);
+app.use('/api/todo', auth);
+app.use('/api/todo', user);
 
 app.listen(port, () => {
   console.log(`Connection is setup at ${port}`);
